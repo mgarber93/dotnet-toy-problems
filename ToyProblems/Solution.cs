fe7dafb9e08f6e7009902217e2c365a3c8a0f5a1
mgarber93 <mgarber93@gmail.com>
2022-10-05 21:31:09 -0400

    Add solution

---
+++ ToyProblems/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ToyProblems.Test")]
+namespace ToyProblems;
+internal static class Solution
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
